3cb789ca5c765b6268b958cb429530f8dc5beee6
montassar <montassarahmed68@gmail.com>
2023-06-04 19:16:20 +0100

    v1.1.6

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ProductService {
-  public apiUrl = 'http://192.168.1.15:8080';
+  public apiUrl = 'http://localhost:8080';
 /*   private signalements: any[] = [
     { id: 1, datePanne: '2022-01-01', numSerie: 'ABC123', typePanne: 'Tête', repare: "OUI", nomProduit: 'Produit 1', observations: 'Observations du signalement 1' },
     { id: 2, datePanne: '2022-02-01', numSerie: 'DEF456', typePanne: 'Mandrin', repare: "NON", nomProduit: 'Produit 2', observations: 'Observations du signalement 2' },
